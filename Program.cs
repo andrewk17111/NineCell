b2e0a3b1d82207f0e5a78bffd155c9b320af9ed1
andrewk17111 <andrewk17111@gmail.com>
2023-03-28 14:24:44 -0400

    Add Number Input
    The user is now able to input numbers into the sudoku board

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using NineCell;
+using System.Text.RegularExpressions;
 
-Console.WriteLine(new Board());
+Board board = new Board();
+
+Console.WriteLine(board);
 Console.SetCursorPosition(1, 1);
 
 while (true)
@@ -10,7 +13,7 @@ while (true)
     ProcessInput(key_info.Key);
 }
 
-static void ProcessInput(ConsoleKey key)
+void ProcessInput(ConsoleKey key)
 {
     if (key == ConsoleKey.UpArrow && Console.CursorTop > 1)
     {
@@ -40,4 +43,31 @@ static void ProcessInput(ConsoleKey key)
         if (Console.CursorLeft % 4 == 0)
             Console.CursorLeft += 1;
     }
+    else if ((key >= ConsoleKey.D0 && key <= ConsoleKey.D9) || (key >= ConsoleKey.NumPad0 && key <= ConsoleKey.NumPad9))
+    {
+        int value = GetValueFromKey(key);
+        (int x, int y) = ConvertPosition(Console.CursorLeft, Console.CursorTop);
+
+        if (value == 0)
+        {
+            board.SetCell(x, y, 0);
+            Console.Write(" ");
+            Console.CursorLeft -= 1;
+        }
+        else if (board.TrySetCell(x, y, value))
+        {
+            Console.Write(value);
+            Console.CursorLeft -= 1;
+        }
+    }
 }
+
+static int GetValueFromKey(ConsoleKey key)
+    => key >= ConsoleKey.D0 && key <= ConsoleKey.D9
+        ? key - ConsoleKey.D0
+        : key >= ConsoleKey.NumPad0 && key <= ConsoleKey.NumPad9
+            ? key - ConsoleKey.NumPad0
+            : 0;
+
+static (int, int) ConvertPosition(int x, int y)
+    => (x - x / 4 - 1, y - y / 4 - 1);
