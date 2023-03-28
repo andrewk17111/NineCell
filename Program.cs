285dd558e8e56a062eb0aa1d565cc2d760a9fc46
andrewk17111 <andrewk17111@gmail.com>
2023-03-28 14:02:20 -0400

    Add Board Movement
    Added the ability to move around the board using arrow keys

--- Program.cs
+++ Program.cs
@@ -1,3 +1,43 @@
 ﻿using NineCell;
 
 Console.WriteLine(new Board());
+Console.SetCursorPosition(1, 1);
+
+while (true)
+{
+    ConsoleKeyInfo key_info = Console.ReadKey(true);
+
+    ProcessInput(key_info.Key);
+}
+
+static void ProcessInput(ConsoleKey key)
+{
+    if (key == ConsoleKey.UpArrow && Console.CursorTop > 1)
+    {
+        Console.CursorTop -= 1;
+
+        if (Console.CursorTop % 4 == 0)
+            Console.CursorTop -= 1;
+    }
+    else if (key == ConsoleKey.DownArrow && Console.CursorTop < 11)
+    {
+        Console.CursorTop += 1;
+
+        if (Console.CursorTop % 4 == 0)
+            Console.CursorTop += 1;
+    }
+    else if (key == ConsoleKey.LeftArrow && Console.CursorLeft > 1)
+    {
+        Console.CursorLeft -= 1;
+
+        if (Console.CursorLeft % 4 == 0)
+            Console.CursorLeft -= 1;
+    }
+    else if (key == ConsoleKey.RightArrow && Console.CursorLeft < 11)
+    {
+        Console.CursorLeft += 1;
+
+        if (Console.CursorLeft % 4 == 0)
+            Console.CursorLeft += 1;
+    }
+}
