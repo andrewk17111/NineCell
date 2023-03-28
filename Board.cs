c0b0170564145d69970aceb14e2f25dde0faba8c
andrewk17111 <andrewk17111@gmail.com>
2023-03-27 23:55:14 -0400

    Create Basic Board
    Created a basic sudoku board that can get and set cells as well as be displayed

---
+++ Board.cs
@@ -0,0 +1,63 @@
+﻿namespace NineCell;
+
+internal class Board
+{
+    private readonly int[,] _board;
+
+    public Board()
+    {
+        _board = InitBoard();
+    }
+
+    private static int[,] InitBoard()
+        => new int[,]
+        {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+    public int GetCell(int x, int y)
+        => _board[x, y];
+
+    public void SetCell(int x, int y, int value)
+        => _board[x, y] = value;
+
+    public override string ToString()
+    {
+        string output = "┌───┬───┬───┐\n│";
+
+        for (int y = 0; y < _board.GetLength(1); y++)
+        {
+            for (int x = 0; x < _board.GetLength(0); x++)
+            {
+                output += _board[x, y];
+
+                if (x % 3 == 2)
+                {
+                    output += "│";
+
+                    if (x == _board.GetLength(0) - 1)
+                    {
+                        output += "\n";
+
+                        if (y % 3 != 2)
+                            output += "│";
+                    }
+                }
+            }
+
+            if (y % 3 == 2 && y != _board.GetLength(1) - 1)
+                output += "├───┼───┼───┤\n│";
+        }
+
+        output += "└───┴───┴───┘";
+        return output;
+    }
+}
