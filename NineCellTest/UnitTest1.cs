36ad2f08eecaa941532dd4c0f1d6ce9bcbb14bed
andrewk17111 <andrewk17111@gmail.com>
2023-07-13 20:46:10 -0400

    Add Sudoku Tests
    Add a test to go through test boards

---
+++ NineCellTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualBasic.FileIO;
+
+namespace NineCellTest;
+
+[TestClass]
+public class TestSudokuSolver
+{
+    [TestMethod]
+    public void TestSolver()
+    {
+        
+    }
+}
